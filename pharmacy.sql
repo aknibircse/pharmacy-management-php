fe0c29518d5eb5c2bbf05addd5a4388dc6dc92ff
aknibir-cse <aknibir.cse@gmail.com>
2023-05-27 16:05:15 +0600

    branch-added

--- pharmacy.sql
+++ pharmacy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 26, 2021 at 08:20 AM
+-- Generation Time: Oct 26, 2022 at 08:20 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.33
 
